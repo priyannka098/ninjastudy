cd2b0c3b16d11f829f0eae19878fc0e4b26cd4fe
Priyanka <priyanka.1711kumari@gmail.com>
2022-10-07 03:05:19 +0530

    Fixing the queries section

--- src/Components/QueriesComponent/Queries.jsx
+++ src/Components/QueriesComponent/Queries.jsx
@@ -6,19 +6,22 @@ function Queries(){
 
 return(
     <>
-    <div id="contact" className="container-fluid bg-color-fifth text-dark   ">
+    <div id="contact" className="container-fluid bg-color-fifth text-dark d-flex" style={{justifyContent:"center"}}>
+        <div className="d-flex flex-column" style={{width:"80%"}}>
         <div className="heading-box2">
-        <span className=" display-4 font-weight-bold" style={{fontWeight:"500"}}>Any Queries? Ask us </span>
+        <span className=" display-4 font-weight-bold" style={{fontWeight:"500"}}>Any Queries? </span>
+        <span className=" display-4 font-weight-bold" style={{fontWeight:"500"}}> Ask us </span>
         </div>
-        <div className="d-flex flex-row"style={{justifyContent:"space-around"}}>
-        <div style={{width:"50%"}}>
+        <div className="d-flex flex-row"style={{justifyContent:"space-between"}}>
+        
+        <div className="form-big">
             <form>
                 <div className="askbox1">
-                    <div className="d-flex flex-column" style={{marginRight:"2rem"}}>
+                    <div className="d-flex flex-column" style={{marginRight:"2rem", width:"40%"}}>
                         <label for="name" className="">Enter your name:</label>
                         <input type="text"  className="input-box" id="name" placeholder="enter your name"></input>
                     </div>
-                    <div className="d-flex flex-column" >
+                    <div className="d-flex flex-column" style={{ width:"40%"}} >
                         <label for="email">Enter your email:</label>
                         <input type="text" className="input-box" id="email"  placeholder=" Enter your email"></input>
                     </div>
@@ -34,13 +37,36 @@ return(
             </div>
             </form>
         </div>
+
+        <div className="form-small">
+            
+                    <div className="d-flex flex-column"> 
+                        <label for="name" className="">Enter your name:</label>
+                        <input type="text"  className="input-box" id="name" placeholder="enter your name"></input>
+                    </div> 
+                    <div className="d-flex flex-column" >
+                        <label for="email">Enter your email:</label>
+                        <input type="text" className="input-box" id="email"  placeholder=" Enter your email"></input>
+                    </div>
+                <div className="textarea-div">
+                <label for="textarea">Enter the message:</label>
+                <textarea id="w3review" className="textarea1" name="textarea" rows="3">At w3schools.com you will learn how to make a website. They offer free tutorials in all web development technologies.</textarea> 
+                </div>
+                <div className="submit">
+                    <button type="submit" className="btn btn-dark submit-btn">Submit</button>
+                </div>
+
+ 
+        </div>
+
+
         <div className="ask-image">
         <img src={require("../../Images/askgif.gif")} class="img-fluid queries-img"  alt="Responsive image"></img>
         </div> 
         </div>
     
 </div>
-
+</div>
 
     </>
 
