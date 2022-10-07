093fcf7b1e8ca9d3e20d745de95e8fd531480753
Priyanka <priyanka.1711kumari@gmail.com>
2022-10-07 18:32:48 +0530

    make website responsive

--- src/Components/FooterComponent/Footer.jsx
+++ src/Components/FooterComponent/Footer.jsx
@@ -10,6 +10,8 @@ function Features()
     <>
     <footer>
       <div  className="container-fluid bg-color-forth">
+
+        <div className="footer-big">
         <div className="innerbox">
         <div className="inner-left-box">
         <div className="img-box">
@@ -54,7 +56,7 @@ function Features()
       </div>
       </div>
   
-      <hr style={{width:"80% ", opacity:"1"}}></hr>
+      <hr style={{ opacity:"1"}}></hr>
 
       <div className="bottom-box d-flex flex-row">
         <div className="text-style">
@@ -65,11 +67,83 @@ function Features()
       <p>	&copy; 2022 All right reserved.</p>
       </div>
       </div>
+    </div>
+
+    {/*  Footer clone*/}
+
+<div className="footer-small">
+       <div className="innerbox">
+        <div className="inner-left-box">
+        <div className="img-box">
       
-
-
+            <img src={require("../../Images/logo.png")} class="img-logo"  alt="Responsive image"></img>
+            <span>ninjastudy</span>
+            <div>
+            <span style={{fontSize:"10px"}}> Backed By</span>
+            <img src={require("../../Images/Y-combinator.png")} class="img-y-logo"  alt="Responsive image"></img>
+            </div>
       </div>
+      <div className="footer-box1">  
+            <p className="f-para">NinjaStudy ia a personal AI English tutor App,focused on improving your speaking skills.You can think of it as Siri/Alexa for English learners.</p>
+      </div>
+      {/* <div className="image-icon-box ">
+      <img src={require("../../Images/linkedin.png")} class="img-fluid img-icon " alt="Responsive image"></img>
+      <img src={require("../../Images/twitter.png")} class="img-fluid img-icon " alt="Responsive image"></img>
+      <img src={require("../../Images/instagram.png")} class="img-fluid img-icon" alt="Responsive image"></img>
+      <img src={require("../../Images/youtube.png")} class="img-fluid img-icon" alt="Responsive image"></img>
+      </div> */}
+    </div>
+      <div className="inner-right-box d-flex flex-row">
+        <div>
+      <div className="footer-box2 d-flex flex-column">
+         <p><b> Privacy and terms</b></p>
+          <p> Community Guideliness</p>
+          <p>Terms</p>
+           <p>Privacy</p>
+      </div>
+      <div className="footer-box3 d-flex flex-column">
+          <p><b>Help and Support</b></p>
+          <p>Contact Us</p>
+          <p>FAQ</p>
+          <p>Blog</p>
+      </div>
+      </div>
+      <div>
+      <div className="footer-box4 d-flex flex-column">
+        <p><b>About Us</b></p>
+        <p>Mission</p>
+        <p>Carrers</p>
+        <p>Press</p>
+        <p>Investors</p>
+        </div>
+      </div>
+      </div>
+      </div>
+
+      <div className="image-icon-box ">
+      <img src={require("../../Images/linkedin.png")} class="img-fluid img-icon " alt="Responsive image"></img>
+      <img src={require("../../Images/twitter.png")} class="img-fluid img-icon " alt="Responsive image"></img>
+      <img src={require("../../Images/instagram.png")} class="img-fluid img-icon" alt="Responsive image"></img>
+      <img src={require("../../Images/youtube.png")} class="img-fluid img-icon" alt="Responsive image"></img>
+      </div>
+  
+      <hr style={{width:"100% ", opacity:"1"}}></hr>
+
+      <div className="bottom-box d-flex flex-row">
+        <div className="text-style">
+       <p>Term & Condition</p> 
+      <p style={{marginLeft:"1rem"}} >Privacy & Policy</p>
+      </div>
+      <div  className="right-reserved-styling">
+      <p>	&copy; 2022 All right reserved.</p>
+      </div>
+      </div>
+      </div>
+      </div>
+
+
       </footer>
+
       </>
       
       
--- src/Components/QueriesComponent/Queries.jsx
+++ src/Components/QueriesComponent/Queries.jsx
@@ -7,7 +7,7 @@ function Queries(){
 return(
     <>
     <div id="contact" className="container-fluid bg-color-fifth text-dark d-flex" style={{justifyContent:"center"}}>
-        <div className="d-flex flex-column" style={{width:"80%"}}>
+        <div className="d-flex flex-column queries-parent">
         <div className="heading-box2">
         <span className=" display-4 font-weight-bold" style={{fontWeight:"500"}}>Any Queries? </span>
         <span className=" display-4 font-weight-bold" style={{fontWeight:"500"}}> Ask us </span>
@@ -17,7 +17,7 @@ return(
         <div className="form-big">
             <form>
                 <div className="askbox1">
-                    <div className="d-flex flex-column" style={{marginRight:"2rem", width:"40%"}}>
+                    <div className="d-flex flex-column " style={{marginRight:"2rem", width:"40%"}}>
                         <label for="name" className="">Enter your name:</label>
                         <input type="text"  className="input-box" id="name" placeholder="enter your name"></input>
                     </div>
@@ -41,25 +41,19 @@ return(
         <div className="form-small">
             
                     <div className="d-flex flex-column"> 
-                        <label for="name" className="">Enter your name:</label>
-                        <input type="text"  className="input-box" id="name" placeholder="enter your name"></input>
+                        <label for="name" className="input-text-style">Enter your name:</label>
+                        <input type="text"  className="input-box" id="name" placeholder="Full name"></input>
                     </div> 
-                    <div className="d-flex flex-column" >
+                    <div className="d-flex flex-column" style={{marginTop:"1rem"}}> 
                         <label for="email">Enter your email:</label>
-                        <input type="text" className="input-box" id="email"  placeholder=" Enter your email"></input>
+                        <input type="text" className="input-box" id="email"  placeholder=" Email"></input>
                     </div>
                 <div className="textarea-div">
                 <label for="textarea">Enter the message:</label>
-                <textarea id="w3review" className="textarea1" name="textarea" rows="3">At w3schools.com you will learn how to make a website. They offer free tutorials in all web development technologies.</textarea> 
+                <textarea id="textarea" className="textarea1" name="textarea" rows="3" placeholder="Message"></textarea> 
                 </div>
-                <div className="submit">
-                    <button type="submit" className="btn btn-dark submit-btn">Submit</button>
-                </div>
-
- 
+                <button type="submit" className="btn btn-dark submit-btn submit-btn-small">Submit</button>
         </div>
-
-
         <div className="ask-image">
         <img src={require("../../Images/askgif.gif")} class="img-fluid queries-img"  alt="Responsive image"></img>
         </div> 
